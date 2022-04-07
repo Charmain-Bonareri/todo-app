2173e785ff365b6a8e176942bb10cba672102338
Charmain <mogerecharmain@gmail.com>
2022-04-07 12:41:40 +0300

    Added a div for the todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //shows how to use the component
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] //array we can have multiple
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = 'Todo List'; //will contain data and behaviour
+  todo = {
+          label: 'Bring Milk',
+          done: false,
+          priority:3}; //created an object and then indent it so that it is more readable
 }
